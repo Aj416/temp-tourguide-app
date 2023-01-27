3356eaed3cfa7cca06ca93ffe3789c9bcc6bb478
aj416 <ajay.ab88@gmail.com>
2023-01-27 15:51:52 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,131 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
+
+export const pageLinks = [
+  {
+    id: 1,
+    href: "#home",
+    text: "home",
+  },
+  {
+    id: 2,
+    href: "#about",
+    text: "about",
+  },
+  {
+    id: 3,
+    href: "#services",
+    text: "services",
+  },
+  {
+    id: 4,
+    href: "#tours",
+    text: "tours",
+  },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 4,
+    icon: "fas fa-bed fa-fw",
+    title: "plenty rest",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 5,
+    icon: "fas fa-hospital fa-fw",
+    title: "medical support",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    href: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "china",
+    duration: "6 days",
+    price: "₹ 2100",
+  },
+  {
+    id: 2,
+    href: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: "12 days",
+    price: "₹ 1400",
+  },
+  {
+    id: 3,
+    href: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: "6 days",
+    price: "₹ 1000",
+  },
+  {
+    id: 4,
+    href: tour4,
+    date: "december 12th, 2020",
+    title: "jungle highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "australia",
+    duration: "15 days",
+    price: "₹ 2400",
+  },
+  {
+    id: 5,
+    href: tour5,
+    date: "august 26th, 2020",
+    title: "Jungle Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "china",
+    duration: "6 days",
+    price: "₹ 2100",
+  },
+  {
+    id: 6,
+    href: tour6,
+    date: "december 16th, 2021",
+    title: "Taj Experience",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "india",
+    duration: "10 days",
+    price: "₹ 1500",
+  },
+];
